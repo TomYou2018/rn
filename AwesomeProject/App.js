2c3b527039c77e2de90a805c5221b6b3e58005ed
Tom <18938961795@189.cn>
2018-06-03 23:51:55 +0800

    almost done

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -7,8 +7,9 @@ export default class SectionListBasics extends Component {
       <View style={styles.container}>
         <SectionList
           sections={[
-            {title: 'D', data: ['Devin']},
-            {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+
+//            {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+{title: 'J', data: getUserList()},
           ]}
           renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
           renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
@@ -17,7 +18,25 @@ export default class SectionListBasics extends Component {
     );
   }
 }
+//function getUserList() {
+//  return ['test', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']
+//  }
+//function getUserList() {
+//  return fetch('http://localhost:8080/user/brw')
+//    .then((response) => response.json())
+//    .then((responseJson) => {
+//      return responseJson.users;
+//    })
+//    .catch((error) => {
+//      console.error(error);
+//    });
+//}
 
+function getUserList() {
+  return fetch('http://localhost:8080/user/brw').then(function(response){return ['1','2']);}
+//return ['{"id":1,"name":"Tom","password":"no_pass"}','{"id":2,"name":"Jerry","password":"no_pass"}'];
+//return {"id":1,"name":"Tom","password":"no_pass"},{"id":2,"name":"Jerry","password":"no_pass"};
+}
 const styles = StyleSheet.create({
   container: {
    flex: 1,
