d11be052f790580e361ddd74c72292fc89194c51
TomYou2018 <35715313+TomYou2018@users.noreply.github.com>
2018-06-03 20:09:44 +0800

    sofarsogood

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -20,24 +20,77 @@ const instructions = Platform.select({
 });
 
 type Props = {};
-export default class App extends Component<Props> {
+//export default class HelloWorldApp extends Component {
+//  render() {
+//    return (
+//      <Text>Hello world1!</Text>
+//    );
+//  }
+//}
+//export default class Bananas extends Component {
+//  render() {
+//    let pic = {
+//      uri: 'http://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+//    };
+//    return (
+//      <Image source={pic} style={{width: 193, height: 110}} />
+//    );
+//  }
+////}
+//class Greeting extends Component {
+//  render() {
+//    return (
+//      <Text>Hello {this.props.name}!</Text>
+//    );
+//  }
+//}
+//
+//export default class LotsOfGreetings extends Component {
+//  render() {
+//    return (
+//      <View style={{alignItems: 'center'}}>
+//        <Greeting name='Rexxar' />
+//        <Greeting name='Jaina' />
+//        <Greeting name='Valeera' />
+//      </View>
+//    );
+//  }
+//}
+
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showText: true };
+
+    // 每1000毫秒对showText状态做一次取反操作
+    setInterval(() => {
+      this.setState(previousState => {
+        return { showText: !previousState.showText };
+      });
+    }, 1000);
+  }
+
   render() {
+    // 根据当前showText的值决定是否显示text内容
+    let display = this.state.showText ? this.props.text : ' ';
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          this is wield.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <Text>{display}</Text>
     );
   }
 }
 
+export default class BlinkApp extends Component {
+  render() {
+    return (
+      <View>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
+      </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
