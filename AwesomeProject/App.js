5afb8e6961ee07b79e0a0b2abdfbf1da75a3f350
TomYou2018 <35715313+TomYou2018@users.noreply.github.com>
2018-06-03 20:17:06 +0800

    flexWidthHeight

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
 
-export default class FixedDimensionsBasics extends Component {
+export default class FlexDimensionsBasics extends Component {
   render() {
     return (
-      <View>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+      // 试试去掉父View中的`flex: 1`。
+      // 则父View不再具有尺寸，因此子组件也无法再撑开。
+      // 然后再用`height: 300`来代替父View的`flex: 1`试试看？
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
 };
-// 注册应用(registerComponent)后才能正确渲染
-// 注意：只把应用作为一个整体注册一次，而不是每个组件/模块都注册
-AppRegistry.registerComponent('AwesomeProject', () => FixedDimensionsBasics);
+
+AppRegistry.registerComponent('AwesomeProject', () => FlexDimensionsBasics);
